570a73db314b2f91ad87baa81464b66ac155b9ff
Aristoteles <arystotelys@gmail.com>
2023-05-25 12:40:42 -0300

    removendo rota adm e organizando db

--- src/router.ts
+++ src/router.ts
@@ -4,7 +4,6 @@ import { createCommentController } from "./controller/createCommentController";
 import { getAllcommentsController } from "./controller/getAllcommentsController";
 import { RefusedCommentController } from "./controller/RefusedCommentController";
 import { AuthorizeCommentController } from "./controller/AuthorizeCommentController";
-import { AuthenticateAdmController } from "./controller/AuthenticateAdmController";
 
 const router = Router()
 
@@ -15,8 +14,6 @@ router.get('/all-comments', new getAllcommentsController().handle)
 router.put('/authorize/:id', new AuthorizeCommentController().handle)
 
 router.delete('/refused/:id', new RefusedCommentController().handle)
-
-router.post('/admin', new AuthenticateAdmController().handle)
 
 router.post('/comment', new createCommentController().handle)
 
